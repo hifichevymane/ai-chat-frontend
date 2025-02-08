27a52f0c14e0884070469cf6f7db1a4df88d0745
hifichevymane <blekbjek@gmail.com>
2025-02-08 17:30:53 +0200

    Input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import ThemeCard from "./components/ThemeCard";
+import Input from "./components/Input";
 
 function App() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-full">
-        <h1 className="text-3xl text-center font-semibold font-secondary w-[401px] pb-6">
+      <div className="flex flex-col justify-center items-center h-full w-fit mx-auto">
+        <h1 className="text-3xl text-center font-semibold font-secondary w-[401px] mb-6">
           Hi, I’m Mamaliga AI Chat Bot How can I help you?
         </h1>
-        <h3 className="text-xl text-primary-500 pb-9">
+        <h3 className="text-xl text-primary-500 mb-9">
           Discover and conquer new information with AI Bot!
         </h3>
-        <div className="flex gap-8">
+        <div className="flex gap-8 mb-[168px]">
           <ThemeCard theme="Fun">Tell me a joke to impress the girl I like</ThemeCard>
           <ThemeCard theme="Work Assistant">Create an online chat website</ThemeCard>
           <ThemeCard theme="AI Teacher">What was the Newton’s third law?</ThemeCard>
         </div>
+        <Input />
       </div>
     </>
   )

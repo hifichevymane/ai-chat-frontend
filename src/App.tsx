dceaece934dc69b687322fb47abd8dbe8390aa7a
hifichevymane <blekbjek@gmail.com>
2025-02-09 14:00:10 +0200

    use sendBtn component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import ThemeCard from "./components/ThemeCard";
 import Input from "./components/Input";
+import SendBtn from "./components/SendBtn";
+
+import { useState } from "react";
 
 function App() {
+  const [isSendBtnActive, setIsSendBtnActive] = useState<boolean>(false);
+
+  const onInput: React.ChangeEventHandler<HTMLInputElement> = ({ target }) => {
+    setIsSendBtnActive(!!target.value)
+  };
+
   return (
     <>
       <div className="flex flex-col justify-center items-center h-full w-fit mx-auto">
@@ -16,7 +25,10 @@ function App() {
           <ThemeCard theme="Work Assistant">Create an online chat website</ThemeCard>
           <ThemeCard theme="AI Teacher">What was the Newton’s third law?</ThemeCard>
         </div>
-        <Input />
+        <div className="flex justify-center w-full gap-2.5">
+          <Input onInput={onInput} />
+          <SendBtn isActive={isSendBtnActive} />
+        </div>
       </div>
     </>
   )

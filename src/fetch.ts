5eefc3b91a8e36ec2edc5291694bea88f7a67eb2
hifichevymane <blekbjek@gmail.com>
2025-02-16 18:27:45 +0200

    env var for backend api url

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,7 +1,7 @@
 import { ofetch } from 'ofetch';
 
 export const api = ofetch.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: `${import.meta.env.VITE_BACKEND_API_URL}/api/v1`,
 
   onRequestError({ request, options, error }) {
     console.error("[fetch request error]", request, options, error);

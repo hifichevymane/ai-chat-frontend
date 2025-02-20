f92ccd9dbc85c83afdbe514a1bdf52d0fed0277a
hifichevymane <blekbjek@gmail.com>
2025-02-20 20:41:06 +0200

    chain input text with input value. Better way to control send btn active using useEffect

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -5,17 +5,19 @@ import Message from "../interfaces/Message";
 import MessagesContainer from "./MessagesContainer";
 
 import { api } from "../fetch";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function MainScreen() {
   const [isSendBtnActive, setIsSendBtnActive] = useState<boolean>(false);
   const [inputText, setInputText] = useState<string>('');
   const [messages, setMessages] = useState<Message[]>([]);
 
+  useEffect(() => {
+    setIsSendBtnActive(!!inputText);
+  }, [inputText]);
+
   const onInput: React.ChangeEventHandler<HTMLInputElement> = ({ target }) => {
-    const trimmedValue = target.value.trim();
-    setInputText(trimmedValue);
-    setIsSendBtnActive(!!trimmedValue);
+    setInputText(target.value.trim());
   };
 
   const onKeyUp: React.KeyboardEventHandler<HTMLElement> = (e) => {
@@ -26,24 +28,24 @@ export default function MainScreen() {
   const addMessage = async () => {
     if (!isSendBtnActive) return;
 
-    setIsSendBtnActive(false);
     setMessages(prevMessages => [...prevMessages, { text: inputText, isUser: true }]);
+    setInputText('');
 
-    // try {
-    //   const { message }: { message: string } = await api('/chat', {
-    //     method: 'POST',
-    //     body: { prompt: inputText },
-    //   });
-    //   console.log(message);
-    // } catch (err) {
-    //   console.error(err);
-    // }
+    try {
+      const { message }: { message: string } = await api('/chat', {
+        method: 'POST',
+        body: { prompt: inputText },
+      });
+      console.log(message);
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   return (
     <div className="flex flex-col justify-center w-full items-center py-8">
       <div className="flex flex-col justify-between items-center h-full w-full">
-        <div className="flex flex-col items-center grow w-full">
+        <div className="flex flex-col items-center grow w-full overflow-scroll">
           {
             messages.length
               ? <MessagesContainer messages={messages} />
@@ -52,7 +54,7 @@ export default function MainScreen() {
         </div>
       </div>
       <div className="flex justify-center w-[60%] gap-2.5 absolute bottom-8" onKeyUp={onKeyUp}>
-        <Input onInput={onInput} />
+        <Input onInput={onInput} value={inputText} />
         <SendBtn isActive={isSendBtnActive} onClick={addMessage} />
       </div>
     </div>

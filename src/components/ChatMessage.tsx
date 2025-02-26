a8515d7c497363ec73bee5d6390699f78d9dcee0
hifichevymane <blekbjek@gmail.com>
2025-02-26 20:16:50 +0200

    add link parsing and use markdown only for llm response

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -1,4 +1,5 @@
 import Markdown from "react-markdown"
+import remarkGfm from "remark-gfm"
 
 interface Props {
   isUser: boolean
@@ -6,9 +7,17 @@ interface Props {
 }
 
 export default function ChatMessage({ isUser, text }: Props) {
+  if (!isUser) {
+    return (
+      <article className='[&_h1]:font-bold [&_h1]:text-2xl [&_a]:underline [&_a]:font-semibold [&_a]:text-blue-500 flex flex-col gap-4 w-fit max-w-4/5 p-4 bg-primary-100 rounded-t-xl rounded-br-xl'>
+        <Markdown remarkPlugins={[remarkGfm]}>{text}</Markdown>
+      </article>
+    );
+  }
+
   return (
-    <article className={`[&_h1]:font-bold [&_h1]:text-2xl flex flex-col gap-4 w-fit max-w-4/5 p-4 bg-primary-100 rounded-t-xl ${isUser ? 'rounded-bl-xl' : 'rounded-br-xl'}`}>
-      <Markdown>{text}</Markdown>
-    </article>
+    <div className='flex flex-col gap-4 w-fit max-w-4/5 p-4 bg-primary-100 rounded-t-xl rounded-bl-xl'>
+      {text}
+    </div>
   )
 }

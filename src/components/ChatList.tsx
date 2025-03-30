a59970f7125156537c3b99790d23f75ecc5ad784
hifichevymane <blekbjek@gmail.com>
2025-03-30 20:38:12 +0300

    create chat button

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -28,12 +28,20 @@ export default function ChatList() {
   }, []);
 
   const onChatClick = (id: string) => navigate(`/${id}`);
+  const onCreateNewChatBtnClick = () => navigate('/');
 
   return (
     <>
-      <h4 className="font-secondary font-bold text-xl text-primary-600">
-        Saved chats
-      </h4>
+      <div className="flex justify-between items-center">
+        <h4 className="font-secondary font-bold text-xl text-primary-600">
+          Saved chats
+        </h4>
+        <button className="text-primary-600 cursor-pointer" title="Create new chat" onClick={onCreateNewChatBtnClick}>
+          <svg xmlns="http://www.w3.org/2000/svg" width={31} height={31} viewBox="0 0 24 24">
+            <path fill="currentColor" d="M11 13H6q-.425 0-.712-.288T5 12t.288-.712T6 11h5V6q0-.425.288-.712T12 5t.713.288T13 6v5h5q.425 0 .713.288T19 12t-.288.713T18 13h-5v5q0 .425-.288.713T12 19t-.712-.288T11 18z"></path>
+          </svg>
+        </button>
+      </div>
       {
         !chats.length
           ? (

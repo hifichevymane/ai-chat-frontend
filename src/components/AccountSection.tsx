c9cd9bf0e0e88998e89f469062063314b23e33b4
hifichevymane <blekbjek@gmail.com>
2025-07-04 22:04:04 +0300

    logout on the avatar button click

--- src/components/AccountSection.tsx
+++ src/components/AccountSection.tsx
@@ -1,9 +1,18 @@
 import Avatar from "./Avatar"
+import { AUTH_TOKEN_KEY } from "../const"
+import { useNavigate } from "@tanstack/react-router"
 
 export default function AccountSection() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem(AUTH_TOKEN_KEY);
+    navigate({ to: "/login" });
+  };
+
   return (
     <div className="flex gap-3.5">
-      <Avatar />
+      <Avatar onClick={handleLogout} />
       <div className="flex flex-col justify-center gap-1 font-secondary text-sm">
         <span className="font-bold">John Doe</span>
         <span className="font-medium">email@email.com</span>

e5f7490133594a7679729ee61a1876f4ebbd6b6f
hifichevymane <blekbjek@gmail.com>
2025-07-04 20:18:22 +0300

    fix the error in the chat list component

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "@tanstack/react-router";
+import { useNavigate } from "@tanstack/react-router";
 import { api } from "../fetch";
 
 import ChatTab from "./ChatTab";
@@ -9,8 +9,11 @@ interface Chat {
   title: string;
 }
 
-export default function ChatList() {
-  const { chatId } = useParams({ from: '/$chatId' });
+interface Props {
+  activeChatId: string;
+}
+
+export default function ChatList({ activeChatId }: Props) {
   const navigate = useNavigate();
   const [chats, setChats] = useState<Chat[]>([]);
 
@@ -54,7 +57,7 @@ export default function ChatList() {
           : (
             <div className="flex flex-col gap-5 my-11 overflow-scroll h-full">
               {chats.map(({ id, title }) => (
-                <ChatTab key={id} id={id} title={title} active={chatId === id} onClick={onChatClick} />
+                <ChatTab key={id} id={id} title={title} active={activeChatId === id} onClick={onChatClick} />
               ))}
             </div>
           )

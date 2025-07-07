c98b72bf8f2742a4a4aef44c762a77cd21d4b522
hifichevymane <blekbjek@gmail.com>
2025-07-07 16:12:22 +0300

    encapsulate logic inside the sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,32 @@
 import AccountSection from "./AccountSection";
 import ChatList from "./ChatList";
+
+import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "@tanstack/react-router";
+
 import { api } from "../fetch";
 import { AUTH_TOKEN_KEY } from "../const";
+import { ChatListItem } from "../interfaces/ChatListItem";
 
 export default function Sidebar() {
   const navigate = useNavigate();
-  const { chatId = null } = useParams({ strict: false });
+  const { chatId } = useParams({ strict: false });
+  const [chats, setChats] = useState<ChatListItem[]>([]);
 
-  const handleLogout = async () => {
+  useEffect(() => {
+    const fetchChats = async () => {
+      try {
+        const chats: ChatListItem[] = await api('/chats');
+        setChats(chats);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchChats();
+  }, []);
+
+  const logout = async () => {
     await api('/auth/logout', {
       method: 'POST'
     });
@@ -16,10 +34,18 @@ export default function Sidebar() {
     navigate({ to: '/login' });
   };
 
+  const navigateToChat = (id: string) => navigate({ to: '/$chatId', params: { chatId: id } });
+  const navigateToHome = () => navigate({ to: '/' });
+
   return (
     <aside className="h-screen w-1/4 bg-primary-100 p-6 flex flex-col">
-      <ChatList activeChatId={chatId} />
-      <AccountSection onAvatarClick={handleLogout} />
+      <ChatList
+        chats={chats}
+        activeChatId={chatId}
+        onChatClick={navigateToChat}
+        onCreateNewChatBtnClick={navigateToHome}
+      />
+      <AccountSection onAvatarClick={logout} />
     </aside>
   );
 }

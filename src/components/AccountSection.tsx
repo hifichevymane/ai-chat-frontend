8de0f101ac7bb940d39853afd06fd15fec1bb732
hifichevymane <blekbjek@gmail.com>
2025-07-07 16:01:09 +0300

    handle logic in sidebar component instead of AccountSection component

--- src/components/AccountSection.tsx
+++ src/components/AccountSection.tsx
@@ -1,22 +1,13 @@
 import Avatar from "./Avatar";
-import { AUTH_TOKEN_KEY } from "../const";
-import { useNavigate } from "@tanstack/react-router";
-import { api } from "../fetch";
 
-export default function AccountSection() {
-  const navigate = useNavigate();
+interface Props {
+  onAvatarClick: () => void;
+}
 
-  const handleLogout = async () => {
-    await api('/auth/logout', {
-      method: 'POST'
-    });
-    localStorage.removeItem(AUTH_TOKEN_KEY);
-    navigate({ to: '/login' });
-  };
-
+export default function AccountSection({ onAvatarClick }: Props) {
   return (
     <div className="flex gap-3.5">
-      <Avatar onClick={handleLogout} />
+      <Avatar onClick={onAvatarClick} />
       <div className="flex flex-col justify-center gap-1 font-secondary text-sm">
         <span className="font-bold">John Doe</span>
         <span className="font-medium">email@email.com</span>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,24 @@
 import AccountSection from "./AccountSection";
 import ChatList from "./ChatList";
+import { useNavigate } from "@tanstack/react-router";
+import { api } from "../fetch";
+import { AUTH_TOKEN_KEY } from "../const";
 
 export default function Sidebar() {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await api('/auth/logout', {
+      method: 'POST'
+    });
+    localStorage.removeItem(AUTH_TOKEN_KEY);
+    navigate({ to: '/login' });
+  };
+
   return (
     <aside className="h-screen w-1/4 bg-primary-100 p-6 flex flex-col">
       <ChatList />
-      <AccountSection />
+      <AccountSection onAvatarClick={handleLogout} />
     </aside>
   );
 }

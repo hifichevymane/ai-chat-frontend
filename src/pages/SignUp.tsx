a6380e541ea28b6e70025502e2e4cb8e6b755af2
hifichevymane <blekbjek@gmail.com>
2025-08-03 16:02:48 +0300

    show hide password button in login and sign up pages

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -22,6 +23,8 @@ type SignUpFormValues = z.infer<typeof signUpValidationSchema>;
 export default function SignUpPage() {
   const navigate = useNavigate();
   const { setAccessToken, setShouldLogoutOnBeforeUnload } = useAuth();
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   const {
     register,
@@ -116,32 +119,88 @@ export default function SignUpPage() {
           </label>
           <label className="flex flex-col gap-1 font-secondary text-primary-500 text-sm" htmlFor="password">
             Password
-            <input
-              id="password"
-              type="password"
-              autoComplete="new-password"
-              className="bg-primary-000 border border-primary-200 rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-primary-300 transition"
-              {...register("password")}
-              aria-invalid={!!errors.password}
-              aria-describedby="password-error"
-              required
-            />
+            <div className="relative bg-primary-000 border border-primary-200 rounded-lg">
+              <input
+                id="password"
+                type={showPassword ? "text" : "password"}
+                autoComplete="new-password"
+                className="w-full h-full border border-primary-200 rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-primary-300 transition"
+                {...register("password")}
+                aria-invalid={!!errors.password}
+                aria-describedby="password-error"
+                required
+                maxLength={45}
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword((prev) => !prev)}
+                className="bg-primary-000 z-10 absolute right-3 top-1/2 -translate-y-1/2 text-primary-500 text-xs focus:outline-none"
+                tabIndex={-1}
+                aria-label={showPassword ? "Hide password" : "Show password"}
+              >
+                {
+                  showPassword
+                    ? (<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
+                      <g fill="none">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m3 3l18 18"></path>
+                        <path fill="currentColor" fillRule="evenodd" d="M5.4 6.23c-.44.33-.843.678-1.21 1.032a15.1 15.1 0 0 0-3.001 4.11a1.44 1.44 0 0 0 0 1.255a15.1 15.1 0 0 0 3 4.111C5.94 18.423 8.518 20 12 20c2.236 0 4.1-.65 5.61-1.562l-3.944-3.943a3 3 0 0 1-4.161-4.161L5.401 6.229zm15.266 9.608a15 15 0 0 0 2.145-3.21a1.44 1.44 0 0 0 0-1.255a15.1 15.1 0 0 0-3-4.111C18.06 5.577 15.483 4 12 4a10.8 10.8 0 0 0-2.808.363z" clipRule="evenodd"></path>
+                      </g>
+                    </svg>)
+                    : (
+                      <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
+                        <g fill="currentColor">
+                          <path d="M14 12a2 2 0 1 1-4 0a2 2 0 0 1 4 0"></path>
+                          <path fillRule="evenodd" d="M12 3C6.408 3 1.71 6.824.378 12C1.71 17.176 6.408 21 12 21s10.29-3.824 11.622-9C22.29 6.824 17.592 3 12 3m4 9a4 4 0 1 1-8 0a4 4 0 0 1 8 0" clipRule="evenodd"></path>
+                        </g>
+                      </svg>
+                    )
+                }
+              </button>
+            </div>
             {errors.password && (
               <span id="password-error" className="text-red-500 text-xs mt-1">{errors.password.message}</span>
             )}
           </label>
           <label className="flex flex-col gap-1 font-secondary text-primary-500 text-sm" htmlFor="confirmPassword">
             Confirm Password
-            <input
-              id="confirmPassword"
-              type="password"
-              autoComplete="new-password"
-              className="bg-primary-000 border border-primary-200 rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-primary-300 transition"
-              {...register("confirmPassword")}
-              aria-invalid={!!errors.confirmPassword}
-              aria-describedby="confirmPassword-error"
-              required
-            />
+            <div className="relative bg-primary-000 border border-primary-200 rounded-lg">
+              <input
+                id="confirmPassword"
+                type={showConfirmPassword ? "text" : "password"}
+                autoComplete="new-password"
+                className="w-full h-full border border-primary-200 rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-primary-300 transition"
+                {...register("confirmPassword")}
+                aria-invalid={!!errors.confirmPassword}
+                aria-describedby="confirmPassword-error"
+                required
+                maxLength={45}
+              />
+              <button
+                type="button"
+                onClick={() => setShowConfirmPassword((prev) => !prev)}
+                className="bg-primary-000 z-10 absolute right-3 top-1/2 -translate-y-1/2 text-primary-500 text-xs focus:outline-none"
+                tabIndex={-1}
+                aria-label={showConfirmPassword ? "Hide password" : "Show password"}
+              >
+                {
+                  showConfirmPassword
+                    ? (<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
+                      <g fill="none">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m3 3l18 18"></path>
+                        <path fill="currentColor" fillRule="evenodd" d="M5.4 6.23c-.44.33-.843.678-1.21 1.032a15.1 15.1 0 0 0-3.001 4.11a1.44 1.44 0 0 0 0 1.255a15.1 15.1 0 0 0 3 4.111C5.94 18.423 8.518 20 12 20c2.236 0 4.1-.65 5.61-1.562l-3.944-3.943a3 3 0 0 1-4.161-4.161L5.401 6.229zm15.266 9.608a15 15 0 0 0 2.145-3.21a1.44 1.44 0 0 0 0-1.255a15.1 15.1 0 0 0-3-4.111C18.06 5.577 15.483 4 12 4a10.8 10.8 0 0 0-2.808.363z" clipRule="evenodd"></path>
+                      </g>
+                    </svg>)
+                    : (
+                      <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
+                        <g fill="currentColor">
+                          <path d="M14 12a2 2 0 1 1-4 0a2 2 0 0 1 4 0"></path>
+                          <path fillRule="evenodd" d="M12 3C6.408 3 1.71 6.824.378 12C1.71 17.176 6.408 21 12 21s10.29-3.824 11.622-9C22.29 6.824 17.592 3 12 3m4 9a4 4 0 1 1-8 0a4 4 0 0 1 8 0" clipRule="evenodd"></path>
+                        </g>
+                      </svg>
+                    )
+                }
+              </button>
+            </div>
             {errors.confirmPassword && (
               <span id="confirmPassword-error" className="text-red-500 text-xs mt-1">{errors.confirmPassword.message}</span>
             )}
